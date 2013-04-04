071c3a2654dbbe3403cfb3eaaf85aa3bdab29fa8
Magnus Holm <judofyr@gmail.com>
2013-04-04 20:58:28 +0200

    Tests: xor keys with @key to clarify

--- test/test_routing_table.rb
+++ test/test_routing_table.rb
@@ -2,7 +2,7 @@ require_relative 'helper'
 
 module Kademlia
   class TestRoutingTable < MiniTest::Unit::TestCase
-    def k(h) Key.from_hex(h) end
+    def k(h) Key.from_hex(h).xor(@key) end
 
     def setup
       @key = Key.from_hex("dead")
@@ -16,15 +16,15 @@ module Kademlia
     end
 
     def test_split
-      @table.store(k("DEA0"), :a)
-      @table.store(k("DEA1"), :b)
-      @table.store(k("2EDA"), :d)
-      @table.store(k("DEA2"), :c)
+      @table.store(k("0001"), :a)
+      @table.store(k("0002"), :b)
+      @table.store(k("F000"), :d)
+      @table.store(k("0003"), :c)
 
       assert @table.bucket.next, "the bucket wasn't split"
       fst = @table.bucket
       assert_equal 1, fst.content.size
-      assert_equal [k("2EDA"), :d], fst.content[0].last
+      assert_equal [k("F000"), :d], fst.content[0].last
     end
   end
 end
